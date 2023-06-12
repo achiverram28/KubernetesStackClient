5a13361b2ac5bf2ff34cb52954473331072cb98a
achiverram28 <ramsamarth21bcs24@iiitkottayam.ac.in>
2023-06-12 20:12:02 +0530

    Modified the full structure and added HelmRepoChartList
    Signed-off-by: achiverram28 <ramsamarth21bcs24@iiitkottayam.ac.in>

--- pkg/helmpkg/helmrepolist.go
+++ pkg/helmpkg/helmrepolist.go
@@ -2,18 +2,30 @@
 package helmpkg
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 
 	"helm.sh/helm/v3/pkg/cli"
+	"helm.sh/helm/v3/pkg/getter"
 	"helm.sh/helm/v3/pkg/repo"
 )
 
 var settings *cli.EnvSettings
 
-func HelmRepoList() {
+type repoFormat struct {
+	Name string
+	URL string
+}
+
+type chartFormat struct {
+	Name string
+	chartVersion string
+	appVersion string
+	Description string
+}
+
+func HelmRepoList() []repoFormat{
 
 	settings = cli.New()
 
@@ -23,14 +35,70 @@ func HelmRepoList() {
 	if err != nil && !os.IsExist(err) {
 		log.Fatal(err)
 	}
-	
+
 	r, err := repo.LoadFile(repoFile)
 	if err != nil {
 		log.Fatalf("Failed to load repository file: %v", err)
 	}
 
+	var repo []repoFormat
 	for _, re := range r.Repositories {
-		fmt.Println(re.Name," ",re.URL)
+		repo = append(repo, repoFormat{Name: re.Name, URL: re.URL})
 	}
 
+	return repo
+
+}
+
+func HelmRepoChartList(repoName string) []chartFormat{
+
+	settings = cli.New()
+
+	repoConfig := settings.RepositoryConfig
+
+    err := os.MkdirAll(filepath.Dir(repoConfig), os.ModePerm)
+	if err != nil && !os.IsExist(err) {
+		log.Fatal(err)
+	}
+
+	repoFile, err := repo.LoadFile(repoConfig)
+	if err != nil {
+		log.Fatalf("Failed to load repository file: %v", err)
+	}
+
+	var chartNames []chartFormat
+
+	flag := false
+
+	for _, rep := range repoFile.Repositories {
+		if rep.Name == repoName {
+		flag = true
+		r, err := repo.NewChartRepository(rep, getter.All(settings))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if _, err := r.DownloadIndexFile(); err != nil {
+			log.Fatal(err)
+		}
+
+		indexFilePath := r.CachePath+"/"+string(repoName)+"-index.yaml"
+		indexFile, err := repo.LoadIndexFile(indexFilePath)
+		if err != nil {
+			log.Fatal(err)
+		}
+        
+		for name := range indexFile.Entries {
+			chartNames = append(chartNames, chartFormat{Name: name, chartVersion: indexFile.Entries[name][0].Version, appVersion: indexFile.Entries[name][0].AppVersion, Description: indexFile.Entries[name][0].Description})
+		}
+
+}
+}
+	if !flag{
+		log.Println("Repo does not exist")
+		return nil
+	}
+
+	return chartNames
+	
 }
